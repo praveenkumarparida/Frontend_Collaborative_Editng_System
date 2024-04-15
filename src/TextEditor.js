e81357ff22236461bb5b4f4a7429db4f55d63b7e
prave <praveenlaehss1@gmail.com>
2024-04-15 10:07:47 +0530

    modified

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState()
 
   useEffect(() => {
-    const s = io("https://backend-collaborative-editng-system.onrender.com")
+    const s = io("http://localhost:3001")
     setSocket(s)
 
     return () => {
